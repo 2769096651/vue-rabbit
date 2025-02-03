4df638a66188d1769b0aa3f93192d3cd4b40cc64
haibo <haibo@163.com>
2025-02-04 00:57:32 +0800

    图片懒加载指令事件

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -8,7 +8,6 @@ const newList = ref([])
 const getNewList = async ()=>{
   const res = await findNewAPI()
   newList.value = res.result
-  console.log(newList.value)
 }
 onMounted(()=>{
   getNewList()
