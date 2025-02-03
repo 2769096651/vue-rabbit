97cdd90a6cfcf2e36e1038c852a069808da8da3a
haibo <haibo@163.com>
2025-02-03 16:04:37 +0800

    layout静态页面

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,10 +1,16 @@
 <template>
 <div class='layout'>
   <h2>我是首页</h2>
+  <LayoutHeader/>
+  <LayoutNav/>
   <RouterView/>
+  <LayoutFooter/>
 </div>
 </template>
 <script setup  name='Layout'>
+import LayoutHeader from './components/LayoutHeader.vue';
+import LayoutNav from './components/LayoutNav.vue';
+import LayoutFooter from './components/LayoutFooter.vue';
 
 
 </script>
