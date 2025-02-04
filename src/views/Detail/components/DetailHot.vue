aed8623ae010681d90edcc407579c9cce9d702e8
haibo <haibo@163.com>
2025-02-05 03:00:50 +0800

    title和列表数据适配

--- src/views/Detail/components/DetailHot.vue
+++ src/views/Detail/components/DetailHot.vue
@@ -4,14 +4,27 @@
 //2.调用接口渲染模板
 import {fetchHotGoodsAPI} from '@/apis/detail'
 import { useRoute } from 'vue-router';
-import {onMounted, ref} from 'vue'
+import {computed, onMounted, ref} from 'vue'
+//设计props参数 适配不同的title和数据
+const props =  defineProps({
+  hotType:{
+    type:Number
+  }
+})
+//适配title 1 - 24小时热榜 2- 周热榜
+const TYPEMAP = {
+  1:'24小时热榜',
+  2:'周热榜'
+}
+
+const title =  computed(()=>TYPEMAP[props.hotType])
 
 const hotList = ref([])
 const route = useRoute()
 const getHotList = async()=>{
   const res =await fetchHotGoodsAPI({
     id:route.params.id,
-    type:1,
+    type:props.hotType,
   })
   hotList.value = res.result
 }
@@ -24,7 +37,7 @@ onMounted(()=>{
 
 <template>
   <div class="goods-hot">
-    <h3>周日榜单</h3>
+    <h3>{{ title }}</h3>
     <!-- 商品区块 -->
     <RouterLink to="/" class="goods-item" v-for="item in hotList" :key="item.id">
       <img :src="item.picture" alt="" />
