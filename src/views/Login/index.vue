67a0064e78a6ab90f46c7c434182531ced6e3194
haibo <haibo@163.com>
2025-02-06 02:15:07 +0800

    表单校验-同意校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -4,8 +4,8 @@ import {ref} from 'vue'
 //表单校验(账号名+密码)
 //准备表单对象
 const form = ref({
-  account: '18610848230',
-  password: '123456',
+  account: '',
+  password: '',
   agree: true
 })
 //
@@ -32,6 +32,17 @@ const rules = {
     }
   ]
 }
+
+const formRef = ref(null)
+const doLogin = ()=>{
+  formRef.value.validate((valid)=>{
+    console.log(valid)
+    //以valid作为判断条件 如果通过检验才执行登录逻辑
+    if(valid){
+      
+    }
+  })
+}
 </script>
 
 
@@ -56,7 +67,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account"/>
@@ -69,7 +80,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
