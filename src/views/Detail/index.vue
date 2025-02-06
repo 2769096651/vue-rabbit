228f0f3295995aa2c2c8648d21ab4cf353736311
haibo <haibo@163.com>
2025-02-07 00:07:35 +0800

    本地购物车-添加购物车实现

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,9 @@ import {getDetail} from '@/apis/detail'
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import DetailHot from './components/DetailHot.vue';
+import { ElMessage } from 'element-plus';
+import { useCartStore } from '@/stores/cartStore';
+const cartStore =useCartStore()
 
 const goods = ref([])
 const route = useRoute()
@@ -14,9 +17,36 @@ onMounted(()=>{
   getGoods()
 })
 //sku规格被操作时候
+let skuObj ={}
 const skuChange = (sku)=>{
   console.log(sku)
+  skuObj = sku
+}
 
+const count = ref(1)
+const handleCount = (count) => {
+  console.log(count)
+}
+
+//添加购物车
+const addCart = () => {
+  if(skuObj.skuId){
+    //规格已经选择 触发action
+    cartStore.addCart({
+      id:goods.value.id,
+      name:goods.value.name,
+      picture:goods.value.mainPictures[0],
+      price:goods.value.price,
+      count:count.value,
+      skuId:skuObj.skuId,
+      atttrsText:skuObj.specsText,
+      selected:true
+    })
+  }else{
+    //规则没有选择 提示用户
+    ElMessage.warning('请选择规则')
+
+  }
 }
 </script>
 
@@ -95,10 +125,10 @@ const skuChange = (sku)=>{
               <!-- sku组件 -->
               <XtxSku :goods="goods" @change="skuChange"/>
               <!-- 数据组件 -->
-
+              <el-input-number v-model="count"  @change="handleCount" />
               <!-- 按钮组件 -->
               <div>
-                <el-button size="large" class="btn">
+                <el-button size="large" class="btn" @click="addCart">
                   加入购物车
                 </el-button>
               </div>
