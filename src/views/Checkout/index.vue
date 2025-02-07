d01c7e0b9ec0101dcc9354af1df9f87c9fa43acf
haibo <haibo@163.com>
2025-02-08 05:32:57 +0800

    支付-获取基础的支付信息

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,6 +1,10 @@
 <script setup>
-import {getCheckInfoAPI} from '@/apis/checkout.js'
+import {getCheckInfoAPI , createOrderAPI} from '@/apis/checkout.js'
+import {useRouter} from 'vue-router';
 import { onMounted, ref } from 'vue';
+import { useCartStore } from '@/stores/cartStore'
+const cartStore = useCartStore()
+const router = useRouter()
 const checkInfo = ref({}) // 订单对象
 const curAddress = ref({}) // 地址对象
 const getCheckInfo =async () =>{
@@ -31,7 +35,31 @@ const confirm = ()=>{
   showDialog.value = false
   activeAddress.value = {}
 }
-
+//创建订单
+const createOrder = async () =>{
+  const res =  await createOrderAPI({
+    deliveryTimeType:1,
+    payType:1,
+    payChannel:1,
+    buyerMessage:1,
+    goods:checkInfo.value.goods.map(item =>{
+      return{
+        skuId:item.skuId,
+        count:item.count
+      }
+    }),
+    addressId : curAddress.value.id
+  })
+  const orderId = res.result.id
+  router.push({
+    path:'/pay',
+    query:{
+      id:orderId
+    }
+  })
+  //更新购物车
+  cartStore.updateNewList()
+}
 
 </script>
 
@@ -127,7 +155,7 @@ const confirm = ()=>{
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large" >提交订单</el-button>
+          <el-button type="primary" size="large" @click="createOrder" >提交订单</el-button>
         </div>
       </div>
     </div>
